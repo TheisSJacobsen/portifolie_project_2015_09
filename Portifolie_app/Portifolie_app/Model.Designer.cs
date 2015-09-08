ca4572a4539f8efbe6265c56bd2544ccb377d27e
allan <katana_kaj@hotmail.com>
2015-09-08 09:34:20 +0200

    MySqlConnection
    Der er forbindelse til MySQL DB

---
+++ Portifolie_app/Portifolie_app/Model.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Crap Inserted\6. Webdeveloper\Excersises\Project\portifolie_project_2015_09\Portifolie_app\Portifolie_app\Model.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
