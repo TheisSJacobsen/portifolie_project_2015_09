12297dfa42441359b02d8865fb9b211b24b85bea
TheisSJacobsen <siehtnesbocaj@gmail.com>
2015-09-15 15:54:36 +0200

    jeg prøvede lige mvc
    Som forslået a læren kunne vi bruge asp.net mvc med EF. Da den allerede
    har en indbygget bruger login system er noget af vores system allerede
    lavet. Derudover kan man scafolde de ting som vi bare skal have listet.
    Syntaxen er meget mere som den bagved (i fronten) og ihvertfald for mig
    giver et større overblik.
    
    Vil gerne høre hvad du syndes om det
    -TSJ

---
+++ PortfolioMVC/PortfolioMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private portifolieDbEntities db = new portifolieDbEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View(db.tblusers.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
