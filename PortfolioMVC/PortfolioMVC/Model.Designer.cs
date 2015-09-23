0afde3b5650d87fa0d97cd4d5099fcfdd1ca72be
TheisSJ <siehtnesbocaj@gmail.com>
2015-09-23 14:16:00 +0200

    ting'

--- PortfolioMVC/PortfolioMVC/Model.Designer.cs
+++ PortfolioMVC/PortfolioMVC/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Theis\Documents\GitHub\portifolie_project_2015_09\PortfolioMVC\PortfolioMVC\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Bruger\Documents\GitHub\portifolie_project_2015_09\PortfolioMVC\PortfolioMVC\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
